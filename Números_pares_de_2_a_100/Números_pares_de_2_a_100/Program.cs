55ea60c0d53cdba3fa12434c2da544fe015a57b2
Brian-loop <brian.vegacz@gmail.com>
2025-03-27 09:53:31 -0300

    Pares_2_a_100
    1)// Mostre somente números pares de 1 a 100

---
+++ Números_pares_de_2_a_100/Números_pares_de_2_a_100/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Números_pares_de_2_a_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //numeros pares de 2 a 100.
+            int contador;
+            for (contador = 2; contador <= 100; contador++)
+            {
+                if (contador % 2 == 0)
+                {
+                 Console.Write(contador + "\n");
+
+                    }
+                }
+            }
+        }
+    }
+            
+        
+     
+
