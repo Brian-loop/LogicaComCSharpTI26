e339dc6b3a82482c475d4a85a207127b7c57e378
Brian-loop <brian.vegacz@gmail.com>
2025-03-27 10:49:05 -0300

    2)impares_até_100
    2)Mostre somente os números ímpares de 1 a 100.

---
+++ 2)numero_impares 1_a_100/2)numero_impares 1_a_100/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_numero_impares_1_a_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2)Mostre somente números impares de 1 a 100.
+            int contadorPares = 0;
+            Console.WriteLine("Números impares de 1 a 100: ");
+            while (contadorPares <= 100) // enquanto(portugol) = white
+            {
+                if (contadorPares % 2 - 1 == 0)//if = para
+                {
+                Console.WriteLine(contadorPares);
+                }
+                contadorPares++;
+            }
+        }
+    }
+}
