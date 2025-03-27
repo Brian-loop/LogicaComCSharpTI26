301d02192f7d8866ad57522527eaafc6fe280f91
Brian-loop <brian.vegacz@gmail.com>
2025-03-27 11:13:25 -0300

    3)20numeros_Mostre_qual_o_maior
    3)Escreva um programa que leia um conjunto de 20 números inteiros e mostre qual foi o maior valor fornecido pelo usuário.

---
+++ 20_numeros_Mostre_ qual_ é_o_maior/20_numeros_Mostre_ qual_ é_o_maior/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_numeros_Mostre__qual__é_o_maior
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3)Escreva um programa que leia um conjunto de 20 números inteiros e mostre qual foi o maior valor fornecido pelo usuário.
+            int num, contador, maiornum;
+            maiornum = -999999;
+            for(contador = 1; contador <= 20; contador++) {   //for = para
+            Console.Write("Digite um número: ");
+                num = int.Parse(Console.ReadLine());
+                // int.Parse(Console.ReadLine()) = leia (portugol)
+            if (num > maiornum) {    //if = se
+            maiornum = num;
+            }
+            }
+            Console.Write("Entre esses 20 números o maior é: ");
+            Console.WriteLine(maiornum);
+        }
+    }
+}
