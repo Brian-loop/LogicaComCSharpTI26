a82c18ac8ccf8ce91edec6d62afb64e32eda50f0
Brian-loop <brian.vegacz@gmail.com>
2025-03-27 09:51:23 -0300

    pares_enquanto
    a

---
+++ Pares_Enquanto/Pares_Enquanto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pares_Enquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1) Mostre somente números pares.
+            int contadorPares = 0;            
+            while(contadorPares <= 100) // enquanto(portugol) = white
+            {
+            if(contadorPares % 2 == 0)
+            {
+            Console.WriteLine(contadorPares);
+            }
+            contadorPares++;
+            }
+        }
+    }
+}
