deb54a4900ccab923511d509ea1fd77497c32b36
Brian-loop <brian.vegacz@gmail.com>
2025-04-02 11:01:23 -0300

    3-reajuste_de5%
    3- Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.

---
+++ 3-Reajuste_de_5%/3-Reajuste_de_5%/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Reajuste_de_5_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3- Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajsute de 5%.
+            double valor, valorReajustado;
+            Console.Write("Digite um valor: ");
+            valor = double.Parse(Console.ReadLine());
+            Console.WriteLine("Valor normal: " + valor);
+            valorReajustado = (valor * 0.05) + valor;
+            Console.WriteLine("Valor reajustado: " + valorReajustado);        
+        }
+    }
+}
