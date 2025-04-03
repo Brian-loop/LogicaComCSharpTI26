e135ac73e3bb7263beedf3a1d00ef44c5315ebe1
Brian-loop <brian.vegacz@gmail.com>
2025-04-03 10:58:17 -0300

    a

--- 01)valores_A_e_B/01)valores_A_e_B/Program.cs
+++ 01)valores_A_e_B/01)valores_A_e_B/Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _01_valores_A_e_B
 {
@@ -15,7 +9,31 @@ namespace _01_valores_A_e_B
             //1 - Faça um algoritimo que leia dois valores inteiros A e B, se os valores de A e B forem iguais,
             //deverá somar valores, caso contrario deve multiplicar A por B. Ao final qualquer um dos cálculos
             //deve se atribuir o resultado a uma variavel C e imprimir o valor na tela.
+//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+            Console.Title = "SENAC";    //comando para mudar o nome da janela
+            Console.ForegroundColor = ConsoleColor.Red;   //este comando faz dar cor a partir da linha abaixo.
+            // colocamos (@) para as letra ficar da foram que a gente copiou e não em uma linha só.
+            Console.WriteLine("       ╔═════════════════════════════════════════════════════════════════════════════════════════════════════════╗");
+            Console.WriteLine(@"       ║                                                                                                         ║
+       ║   ██▒   █▓ ▄▄▄       ██▓     ▒█████   ██▀███      ██▓ ███▄    █ ▄▄▄█████▓▓█████  ██▓ ██▀███   ▒█████    ║
+       ║  ▓██░   █▒▒████▄    ▓██▒    ▒██▒  ██▒▓██ ▒ ██▒   ▓██▒ ██ ▀█   █ ▓  ██▒ ▓▒▓█   ▀ ▓██▒▓██ ▒ ██▒▒██▒  ██▒  ║
+       ║   ▓██  █▒░▒██  ▀█▄  ▒██░    ▒██░  ██▒▓██ ░▄█ ▒   ▒██▒▓██  ▀█ ██▒▒ ▓██░ ▒░▒███   ▒██▒▓██ ░▄█ ▒▒██░  ██▒  ║
+       ║    ▒██ █░░░██▄▄▄▄██ ▒██░    ▒██   ██░▒██▀▀█▄     ░██░▓██▒  ▐▌██▒░ ▓██▓ ░ ▒▓█  ▄ ░██░▒██▀▀█▄  ▒██   ██░  ║
+       ║     ▒▀█░   ▓█   ▓██▒░██████▒░ ████▓▒░░██▓ ▒██▒   ░██░▒██░   ▓██░  ▒██▒ ░ ░▒████▒░██░░██▓ ▒██▒░ ████▓▒░  ║
+       ║     ░ ░░    ▒   ▒▒ ░░ ░ ▒  ░  ░ ▒ ▒░   ░▒ ░ ▒░    ▒ ░░ ░░   ░ ▒░    ░     ░ ░  ░ ▒ ░  ░▒ ░ ▒░  ░ ▒ ▒░   ║
+       ║     ░ ▐░   ▒▒   ▓▒█░░ ▒░▓  ░░ ▒░▒░▒░ ░ ▒▓ ░▒▓░   ░▓  ░ ▒░   ▒ ▒   ▒ ░░   ░░ ▒░ ░░▓  ░ ▒▓ ░▒▓░░ ▒░▒░▒░   ║
+       ║       ░░    ░   ▒     ░ ░   ░ ░ ░ ▒    ░░   ░     ▒ ░   ░   ░ ░   ░         ░    ▒ ░  ░░   ░ ░ ░ ░ ▒    ║
+       ║        ░        ░  ░    ░  ░    ░ ░     ░         ░           ░             ░  ░ ░     ░         ░ ░    ║
+       ║       ░                                                                                                 ║
+       ║                                                                                                         ║");
+            Console.Write("       ╚═════════════════════════════════════════════════════════════════════════════════════════════════════════╝");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;  //este comando faz dar cor a partir da linha abaixo.
+            Console.WriteLine(" 1 - Faça um algoritimo que leia dois valores inteiros A e B, se os valores de A e B forem iguais,deverá somar valores, caso contrario deve multiplicar A por B. Ao final qualquer um dos cálculos\r\ndeve se atribuir o resultado a uma variavel C e imprimir o valor na tela.");
+            Console.ResetColor();   //comando para a parar de dar cor.
             int numA, numB, numC;
+
+            Console.WriteLine();
             Console.Write("Digite seu primeiro número: ");
             numA = int.Parse(Console.ReadLine());
             Console.Write("Digite um segundo número:");
@@ -24,11 +42,13 @@ namespace _01_valores_A_e_B
             {
                 numC = numA + numB;
             }
-                else
+            else
             {
                 numC = numA * numB;
             }
+            Console.ForegroundColor= ConsoleColor.Yellow;
             Console.WriteLine("O resultado é: " + numC);
+            Console.ResetColor();
         }
     }
 }
---
+++ 7-AB_troca/7-AB_troca/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_AB_troca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //7 - Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.
+            int valorA, valorB;
+            Console.WriteLine("Digite um número: ");
+            valorA = int.Parse(Console.ReadLine());
+
+            
+
+        }
+    }
+}
