1b38898a9f444094db920c7b8146f477f4fa9f66
Brian-loop <brian.vegacz@gmail.com>
2025-04-03 11:32:45 -0300

    a
    a

--- 2-salario_minimo/2-salario_minimo/Program.cs
+++ 2-salario_minimo/2-salario_minimo/Program.cs
@@ -14,13 +14,20 @@ namespace _2_salario_minimo
 
             //2- Faça um algoritimo que leia o valor do sálario mínimo e o valor do sálario de um usuário,
             //calcule quantos sálarios esse usuário ganha e imprima na tela o resultado. (Base para o sálario mínimo R$ 1518,00).
+
+            Console.Title = "Salario minimo";
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+
+
+
             double salarioMinimo = 1518.00;
             // Lendo o valor do salário do usuário
             Console.Write("Digite o valor do seu salário: R$ ");
             double salarioUsuario = Convert.ToDouble(Console.ReadLine());
             // Calculando quantos salários mínimos o usuário ganha
             double quantidadeSalarial = salarioUsuario / salarioMinimo;            
-            Console.WriteLine($"Você ganha {quantidadeSalarial:F2} salários mínimos.");
+            Console.WriteLine($"Você ganha {quantidadeSalarial:F2} a mais de salários mínimos.");
         }
     }
 }
