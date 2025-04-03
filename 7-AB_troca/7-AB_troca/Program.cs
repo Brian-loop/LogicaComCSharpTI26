f84816eb33181deff7a10e50ca5a95497399b084
Brian-loop <brian.vegacz@gmail.com>
2025-04-03 11:32:00 -0300

    7-A_B_troca_valores
    7 - Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.

--- 7-AB_troca/7-AB_troca/Program.cs
+++ 7-AB_troca/7-AB_troca/Program.cs
@@ -11,12 +11,29 @@ namespace _7_AB_troca
         static void Main(string[] args)
         {
             //7 - Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.
+            Console.Title = "7 - Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.";
             int valorA, valorB;
-            Console.WriteLine("Digite um número: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Digite um valor(A): ");
+            Console.ForegroundColor= ConsoleColor.White;
             valorA = int.Parse(Console.ReadLine());
-
-            
-
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.Write("Digite um segundo valor(B): ");
+            Console.ForegroundColor = ConsoleColor.White;
+            valorB = int.Parse(Console.ReadLine());
+            int temp = valorA;
+            valorA = valorB;
+            valorB = temp;
+            // Imprime os valores trocados
+            Console.ForegroundColor= ConsoleColor.Yellow;            
+            Console.Write("O novo valor de A é: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(valorA);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("O novo valor de B é: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(valorB);
+            Console.ResetColor();
         }
     }
 }
