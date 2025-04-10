8588c29af82cb14691f2641fa6874589c0e1746b
Brian-loop <brian.vegacz@gmail.com>
2025-04-10 11:22:28 -0300

    album

---
+++ Cadastro_de_album/Cadastro_de_album/CadastroAlbum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro_de_album
+{
+    internal class CadastroAlbum
+    {
+        public static void CadAlbum()
+        {
+            Console.Write("Digite o nome do álbum: ");
+            string nomeAlbum = Console.ReadLine();
+
+            Console.Write($"Quantas músicas deseja cadastrar no álbum '{nomeAlbum}'? ");
+            int numMusicas = int.Parse(Console.ReadLine());
+
+            List<string> musicas = new List<string>();
+
+        }
+    }
+}
--- Cadastro_de_album/Cadastro_de_album/Program.cs
+++ Cadastro_de_album/Cadastro_de_album/Program.cs
@@ -12,23 +12,48 @@ namespace Cadastro_de_album
         {
             //Crie um sistema de cadastro de albuns de um artista/banda em que será possivel cadastrar um número X de músicas informada 
             //pelo usuário. Crie um menu de opções para isso.
-            int musica = 1;
-            while (musica != 0)
+            int opcao = 1;
+            while (opcao != 0)
             {
-                Console.Clear();
+
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine(@"
+                Console.WriteLine(@" 
 █▀▀ ▄▀█ █▀▄ ▄▀█ █▀ ▀█▀ █▀█ █▀▀   █▀ █░█ ▄▀█   █▀▄▀█ █░█ █▀ █ █▀▀ ▄▀█   █
 █▄▄ █▀█ █▄▀ █▀█ ▄█ ░█░ █▀▄ ██▄   ▄█ █▄█ █▀█   █░▀░█ █▄█ ▄█ █ █▄▄ █▀█   ▄");
                 Console.ResetColor();
                 Console.WriteLine();
-                Console.Write("Dígite o nome da música: ");
-                string NomeMusica = Console.ReadLine();
-                Console.WriteLine($"Música {NomeMusica} cadastrada com sucesso!!!");
+                Console.WriteLine("Digite 1 para Cadastrar um album ");
+                Console.WriteLine("Digite 2 para Mostrar albuns e música ");
+                Console.Write("Digite 3 para ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("SAIR");
+                Console.ResetColor();
+                Console.Write("Qual é sua opção: ");
+                while (!int.TryParse(Console.ReadLine(), out opcao || opcao < 0 || opcao > 3)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Opção não correspondente, dígite novamente !");
+                    Console.ResetColor();
+                }
+                switch (opcao)
+                {
 
+                    case 1 :
+                        
+                        break;
+                    case 2 :
 
-                  
+                        break;
+                    case 3 :
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção inválida. Tente novamente.");
+                        break;
+                }
             }
-        }
+        }                
+    
     }
 }
